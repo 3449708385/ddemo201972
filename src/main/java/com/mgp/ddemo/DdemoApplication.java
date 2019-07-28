b16d2ed885e68c1e18bb3a373f792b65cb925951
maoguangping <991335931@qq.com>
2019-07-29 01:10:07 +0800

    在原有的继承上加上部署的命令

--- src/main/java/com/mgp/ddemo/DdemoApplication.java
+++ src/main/java/com/mgp/ddemo/DdemoApplication.java
@@ -17,7 +17,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableAsync
 @EnableScheduling
 public class DdemoApplication extends SpringBootServletInitializer {
-
+	/**
+	 * 部署命令：
+	 * java -jar ddemo.jar 前台指执行，方便查看日志
+	 * java -jar ddemo.jar &   后台执行访问会被唤醒
+	 * nohup java -jar ddemo.jar & 后台执行，日志默认输入当前目录下的nohup.out文件中
+	 * nohup java -jar xxx.jar > catalina.out  2>&1 &  后台执行，日志输入当前目录下的catalina.out文件中
+	 * nohup java -jar xxx.jar >/dev/null &   后台执行，没有日志
+	 *
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DdemoApplication.class, args);
 	}
