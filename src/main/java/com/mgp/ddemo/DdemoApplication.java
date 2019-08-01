f6ba6c843731a6e67e3f1919e9bbc212e31ccd70
maoguangping <991335931@qq.com>
2019-08-01 17:19:07 +0800

    整合docker一键部署，当然需要你到docker创建实例运行（已经成功，但有点小问题，莫名其妙）

--- src/main/java/com/mgp/ddemo/DdemoApplication.java
+++ src/main/java/com/mgp/ddemo/DdemoApplication.java
@@ -19,13 +19,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class DdemoApplication extends SpringBootServletInitializer implements CommandLineRunner {
 	/**
-	 * 部署命令：
+	 * 手动部署命令：
 	 * java -jar ddemo.jar 前台指执行，方便查看日志
 	 * java -jar ddemo.jar &   后台执行访问会被唤醒
 	 * nohup java -jar ddemo.jar & 后台执行，日志默认输入当前目录下的nohup.out文件中
 	 * nohup java -jar xxx.jar > catalina.out  2>&1 &  后台执行，日志输入当前目录下的catalina.out文件中
 	 * nohup java -jar xxx.jar >/dev/null &   后台执行，没有日志
-	 *
+	 * docker部署：(pom文件两种不同的方式：https://www.cnblogs.com/jpfss/p/10945324.html)
+	 * Linux服务器安装doker与项目依赖环境
+	 * 配置docker文件，开启防火墙端口2375，重启docker服务或服务器(https://blog.csdn.net/qq_36850813/article/details/89924207)
+	 * idea安装docker插件(Docker integration)
+	 * mvn clean package docker:build -DpushImage
+	 * 项目push后需要手动创建实例：docker run --name ddemo -d -p8177:8012 ddemo/ddemo ddemo
 	 */
 	@Value("${netty.port}")
 	private int port;
